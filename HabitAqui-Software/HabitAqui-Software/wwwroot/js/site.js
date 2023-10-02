0b01472e2ecd8eea95c47295aeffac8e1a6b3688
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-10-02 17:51:22 +0100

    update _layout.cshtml and many changes in default views

--- HabitAqui-Software/HabitAqui-Software/wwwroot/js/site.js
+++ HabitAqui-Software/HabitAqui-Software/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const toggleButton = document.getElementById('toggleSidebar');
+const sidebar = document.querySelector('.sidebar');
+
+
+let isSidebarVisible = true;
+
+toggleButton.addEventListener('click', () => {
+
+
+    if (isSidebarVisible) {
+        sidebar.style.display = 'none'; // Oculta a barra lateral
+    } else {
+        sidebar.style.display = 'block'; // Mostra a barra lateral
+    }
+
+    isSidebarVisible = !isSidebarVisible;
+});
