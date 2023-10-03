d83540aa0be914fc08ac213c32efbc39e22200fe
Pedro Jorge <pedrorodriguesjorge03@gmail.com>
2023-10-03 18:33:57 +0100

    migrate and create data base

--- HabitAqui-Software/HabitAqui-Software/Data/Migrations/20231003172423_Initial_Migration.Designer.cs
+++ HabitAqui-Software/HabitAqui-Software/Data/Migrations/20231003172423_Initial_Migration.Designer.cs
@@ -4,6 +4,7 @@ using HabitAqui_Software.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HabitAqui_Software.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231003172423_Initial_Migration")]
+    partial class Initial_Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
